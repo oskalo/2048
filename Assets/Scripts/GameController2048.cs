314c9e0ad343da6d72ae2c5d6baa0b893942a917
Oleg-Skalozub <norton47@yandex.by>
2021-05-20 22:00:47 +0300

    work on movements

--- Assets/Scripts/GameController2048.cs
+++ Assets/Scripts/GameController2048.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameController2048 : MonoBehaviour
 {
     [SerializeField] GameObject fillPrefab;
     [SerializeField] Transform[] allCells;
 
+public static Action<string> slide;
     void Start()
     {
         
@@ -18,18 +20,31 @@ public class GameController2048 : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)){
             SpawnFill();
         }
+
+        if(Input.GetKeyDown(KeyCode.W)){
+            slide("w");
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            slide("d");
+        }
+        if(Input.GetKeyDown(KeyCode.A)){
+            slide("a");
+        }
+        if(Input.GetKeyDown(KeyCode.S)){
+            slide("s");
+        }
     }
 
     public void SpawnFill()
     {
-        int whichSpawn = Random.Range(0, allCells.Length);
+        int whichSpawn = UnityEngine.Random.Range(0, allCells.Length);
         if (allCells[whichSpawn].childCount != 0){
             Debug.Log(allCells[whichSpawn].name);
             SpawnFill();
             return;
         }
 
-        float chance = Random.Range(0f, 1f);
+        float chance = UnityEngine.Random.Range(0f, 1f);
         Debug.Log(chance);
         if (chance <.2f){
             return;
@@ -38,12 +53,14 @@ public class GameController2048 : MonoBehaviour
             GameObject tempFill = Instantiate(fillPrefab, allCells[whichSpawn]);
             Debug.Log(2);
             Fill2048 tempFillComp = tempFill.GetComponent<Fill2048>();
+            allCells[whichSpawn].GetComponent<Cell2048>.fill = tempFillComp;
             tempFillComp.FillValueUpdate(2);
         } else
         {
             GameObject tempFill = Instantiate(fillPrefab, allCells[whichSpawn]);
             Debug.Log(4);
             Fill2048 tempFillComp = tempFill.GetComponent<Fill2048>();
+            allCells[whichSpawn].GetComponent<Cell2048>.fill = tempFillComp;
             tempFillComp.FillValueUpdate(4);
         }
     }
